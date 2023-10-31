cd015174ce9d7b3c9c28717520675e62003e0061
EPAM\Aliaksandr_Makhnach <aliaksandr_makhnach@epam.com>
2023-10-31 13:30:01 +0300

    Add project files.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+string dirName = "C:/Users/Aliaksandr_Makhnach/source/repos/FileSystemHomeWork/";
+ 
+var directory = new DirectoryInfo(dirName);
+
+if (directory.Exists)
+{
+    Console.WriteLine("Folders:");
+    DirectoryInfo[] dirs = directory.GetDirectories();
+    foreach (DirectoryInfo dir in dirs)
+    {
+        Console.WriteLine(dir.FullName);
+    }
+    Console.WriteLine();
+    Console.WriteLine("Files:");
+    FileInfo[] files = directory.GetFiles();
+    foreach (FileInfo file in files)
+    {
+        Console.WriteLine(file.FullName);
+    }
+}
